929f4b854c6d020ee530ac7ef39b546adab18816
Cody Rushing <codyrushing@gmail.com>
2026-04-29 21:55:42 -0500

    dance page mobile fix

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   // integrations: [react(), markdoc(), keystatic()],
+  //
+  server: { host: true },
   image: {
     layout: "constrained",
     domains: ['picsum.photos'],
