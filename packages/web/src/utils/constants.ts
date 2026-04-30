c3522e4af7accf5b09f6402b7cbb58318766743d
Cody Rushing <codyrushing@gmail.com>
2026-04-29 21:02:01 -0500

    photos swap. photog credit. dance coming soon page

---
+++ packages/web/src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const punchpassScheduleUrl = 'https://rushtopfish.punchpass.com/classes';
+export const studioMapUrl = 'https://maps.app.goo.gl/PMkfLR2SHiqLSGU17';
+export const pilatesInstagramUrl = 'https://instagram.com/rushtopfish.pilates';
